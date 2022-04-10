b276651ec51e1b347ed61fa1b0e0886d1f0bafdc
Bilal301 <bilalchishti93@gmail.com>
2022-04-10 18:55:53 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Array and for loop
+
+const names = ['anna', 'bob', 'susy'];
+const lastName = 'shakeandbake';
+
+let newArray = [];
+
+// for loop
+
+for (let i = 0; i < names.length; i++) {
+  console.log(i);
+  console.log(names[i]);
+  newArray.push(`${names[i]} ${lastName}`);
+}
+
+console.log(names);
+console.log(newArray);
